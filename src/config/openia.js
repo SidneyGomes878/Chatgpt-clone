889d44d374834dcd5a7f1c170fe6764b7a7089a3
SidneyGomes878 <sidneyerickson09@gmail.com>
2025-08-29 13:11:03 -0300

    first commit

---
+++ src/config/openia.js
@@ -0,0 +1,25 @@
+import OpenAI from "openai";
+
+
+module.exports = class openai {
+
+    static configuration() {
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+
+        return new OpenAI
+    }
+
+    static textCompletion({ prompt }) {
+        return {
+            prompt: `${prompt}`,
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 3500,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        }
+    }
+}
